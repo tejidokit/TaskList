c618a5e95713e0451f0506c366b3a46224432966
Kit Tejido <tejidokit@gmail.com>
2020-07-01 19:17:15 -0600

    boilerplate

--- TaskList/app.js
+++ TaskList/app.js
@@ -5,6 +5,9 @@ const addForm = document.querySelector('.add');
 
 addForm.addEventListener('submit', e => {
 
-})
+    e.preventDefault();
+    const todo = addForm.addEventListener.value;
+    console.log(todo);
+});
 
 //
